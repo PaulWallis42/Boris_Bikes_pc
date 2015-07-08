a0a756484cb9cf42c4be5d74eb73e2fd1f145e30
paulwallis42 <paulwallis42@gmail.com>
2015-07-08 11:51:21 +0100

    spelling correction

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -12,7 +12,7 @@ describe DockingStation do
   it { is_expected.to respond_to(:dock).with(1).argument }
 
     describe 'release_bike' do
-      it 'raises an error with there are no bikes available' do
+      it 'raises an error when there are no bikes available' do
         expect { subject.release_bike }.to raise_error 'No bikes available'
       end
     end
