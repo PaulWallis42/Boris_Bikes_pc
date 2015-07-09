f63600b63f5fe77fe644068be7a0f2b4087f7507
paulwallis42 <paulwallis42@gmail.com>
2015-07-09 09:43:55 +0100

    station capacity created

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,9 +1,13 @@
 require_relative 'bike'
 
 class DockingStation
+  DEFAULT_CAPACITY = 20
+
+  attr_reader :capacity
 
   def initialize
     @bikes = []
+    @capacity = DEFAULT_CAPACITY
   end
 
   def release_bike
@@ -18,12 +22,14 @@ class DockingStation
 
   private
 
+  attr_reader :bikes 
+
   def full?
-    @bikes.count >= 20
+    @bikes.count >= capacity
   end
 
   def empty?
-    @bikes.empty? 
+    @bikes.empty?
   end
 
 end
