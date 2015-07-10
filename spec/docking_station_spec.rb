ebad7cdf5d7dc7ae19fc39e5066b401a615536cb
paulwallis42 <paulwallis42@gmail.com>
2015-07-10 11:29:54 +0100

    test refactor to use doubles

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,14 +4,13 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it 'releases working bikes' do
-    subject.dock Bike.new
-    bike = subject.release_bike
-    expect(bike).to be_working
+    bike = double :bike, broken?: false
+    subject.dock bike
+    expect(subject.release_bike).to be bike
   end
 
   it 'will not release a broken bike' do
-    bike = Bike.new
-    bike.report_broken
+    bike = double :bike, broken?: true
     subject.dock bike
     expect { subject.release_bike }.to raise_error 'No bikes available'
   end
@@ -26,8 +25,9 @@ describe DockingStation do
 
     describe 'dock' do
       it 'raises an error when full' do
-        subject.capacity.times { subject.dock(Bike.new) }
-        expect { subject.dock(Bike.new) }.to raise_error 'Docking Station Full'
+        bike = double :bike, broken?: false
+        subject.capacity.times { subject.dock bike }
+        expect { subject.dock bike }.to raise_error 'Docking Station Full'
       end
 
       it 'can accept a capacity on creation' do
